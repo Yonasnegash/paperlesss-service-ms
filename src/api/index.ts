56fe5a247d7b96322cc810bbeedcfde26ac787fa
brooktewabe <brooktewabe00@gmail.com>
2025-10-31 14:45:56 +0300

    revert base url

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,5 +2,5 @@ import { type Express } from "express";
 
 import router from "./routes";
 export default function initRoutes(app: Express): void {
-  app.use("/v1.0/chatbirrapi/paperless/service", router);
+  app.use("/v1.0/chatbirrapi/paperless", router);
 }
